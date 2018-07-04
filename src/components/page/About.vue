b85fa6d18d132b1455bf0f3ee5afce004cfedc6e
zxb5102 <zxb5102@163.com>
2018-07-04 14:31:36 +0800

    '修改内页的样式修复服务器渲染和浏览器渲染不一致的bug'

--- src/components/page/About.vue
+++ src/components/page/About.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="about common-header-height each-page">
-        <div class="text-hold about-text-hold">
-            南昌盛唐信息科技公司是一家以信息系统集成、信息系统运维、信息技术培训为核心业务的信息科技服务公司，客户遍及国内制造、医疗、金融、教育、通信、信息技术服务、建筑等众多行业。
-盛唐公司于2011年通过ISO27001国际信息安全管理认证，于2012年通过ISO20000信息技术服务管理体系认证，有效促进了盛唐信息技术服务和信息安全管理水平标准化，提高了对客户信息安全和信息服务水平的保障能力。
-盛唐公司凭借专业的技术团队，采用功能集成、网络集成、软件集成等多种集成技术，提供组建企业网、网络安全、数据库高性能集群、数据中心建设、BS软件开发、嵌入式开发等信息系统集成和信息系统运维服务，包括对计算机网络、服务器、存储、小型机等设备，和操作系统、数据库软件平台、虚拟化软件平台等软件的日常维护、系统优化和应急保障，通过对IT运维的标准化，实现IT运行的可靠性，提高企业IT服务的管理水准，满足业务发展对IT基础架构的快速变更和高效管理的要求
-        </div>
+  <div class="about common-header-height each-page">
+    <div class="text-hold about-text-hold">
+      <span class="center-span">
+        南昌盛唐信息科技公司是一家以信息系统集成、信息系统运维、信息技术培训为核心业务的信息科技服务公司，客户遍及国内制造、医疗、金融、教育、通信、信息技术服务、建筑等众多行业。 盛唐公司于2011年通过ISO27001国际信息安全管理认证，于2012年通过ISO20000信息技术服务管理体系认证，有效促进了盛唐信息技术服务和信息安全管理水平标准化，提高了对客户信息安全和信息服务水平的保障能力。 盛唐公司凭借专业的技术团队，采用功能集成、网络集成、软件集成等多种集成技术，提供组建企业网、网络安全、数据库高性能集群、数据中心建设、BS软件开发、嵌入式开发等信息系统集成和信息系统运维服务，包括对计算机网络、服务器、存储、小型机等设备，和操作系统、数据库软件平台、虚拟化软件平台等软件的日常维护、系统优化和应急保障，通过对IT运维的标准化，实现IT运行的可靠性，提高企业IT服务的管理水准，满足业务发展对IT基础架构的快速变更和高效管理的要求
+      </span>
     </div>
+  </div>
 </template>
 <script>
 export default {
   //   mounted() {
   //     this.$store.dispatch("SET_HEADER_FLAG", "about");
   //   },
-  created() {
-    this.$store.dispatch("SET_HEADER_FLAG", "about");
+  asyncData({ store }) {
+    store.dispatch("SET_HEADER_FLAG", "about");
   }
   //   data() {
   //     return {};
@@ -24,7 +24,7 @@ export default {
 .about {
   // height: 500px;
   background-color: gray;
-  background-image: url(/img/page/about/bg.jpg);
+  background-image: url(/img/page/bg.jpg);
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
@@ -35,7 +35,7 @@ export default {
 }
 @media screen and (max-width: 768px) {
   .about-text-hold {
-    position: static;
+    position: relative;
     transform: unset;
     top: unset;
     left: unset;
--- src/components/page/Maintenance.vue
+++ src/components/page/Maintenance.vue
@@ -1,19 +1,20 @@
 <template>
     <div class="maintenance common-header-height each-page">
         <div class="text-hold">
-            信息系统运维，侧重于保障系统正常运行，有运行和维护两层含义。
-盛唐信息系统运维服务，包括对计算机网络、服务器、存储、小型机等设备，和操作系统、数据库软件平台、虚拟化软件平台等软件的日常维护、系统优化和应急保障，通过对IT运维的标准化，实现IT运行的可靠性，提高企业IT服务的管理水准，满足业务发展对IT基础架构的快速变更和高效管理的要求。
+            <span class="center-span">
+                信息系统运维，侧重于保障系统正常运行，有运行和维护两层含义。 盛唐信息系统运维服务，包括对计算机网络、服务器、存储、小型机等设备，和操作系统、数据库软件平台、虚拟化软件平台等软件的日常维护、系统优化和应急保障，通过对IT运维的标准化，实现IT运行的可靠性，提高企业IT服务的管理水准，满足业务发展对IT基础架构的快速变更和高效管理的要求。
+            </span>
         </div>
     </div>
 </template>
 <script>
 export default {
-//   mounted() {
-//     this.$store.dispatch("SET_HEADER_FLAG", "maintenance");
-//   },
-  created() {
-    this.$store.dispatch("SET_HEADER_FLAG", "maintenance");
-        // console.log(22);
+  //   mounted() {
+  //     this.$store.dispatch("SET_HEADER_FLAG", "maintenance");
+  //   },
+  asyncData({ store }) {
+    store.dispatch("SET_HEADER_FLAG", "maintenance");
+    // console.log(22);
   }
 };
 </script>
@@ -21,7 +22,7 @@ export default {
 .maintenance {
   // height: 500px;
   background-color: gray;
-  background-image: url(/img/page/maintenance/bg.jpg);
+  background-image: url(/img/page/bg.jpg);
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
